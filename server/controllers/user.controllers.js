c864987a4b78c2ad37a6617c2ee7b1717fb03117
Sayan-dev731 <sayancodder731@gmail.com>
2026-02-24 18:08:58 +0530

    loginin route with the working of controllers

--- server/controllers/user.controllers.js
+++ server/controllers/user.controllers.js
@@ -3,6 +3,30 @@ import { User } from "../models/user.models.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
+const generateTokens = async (userId) => {
+    try {
+        const user = await User.findById(userId);
+
+        if (!user) {
+            throw new ApiError(404, "Not Found: User not found");
+        }
+
+        const accessToken = user.generateAccessToken();
+        const refreshToken = user.generateRefreshToken();
+
+        user.refreshToken = refreshToken;
+        await user.save({ validateBeforeSave: false });
+
+        return { accessToken, refreshToken };
+    } catch (error) {
+        throw new ApiError(
+            500,
+            error?.message ||
+                "Internal Server Error: Failed to generate tokens."
+        );
+    }
+};
+
 const registerUser = asyncHandler(async (req, res) => {
     // checking if the request coming from is frontend is empty or undefined
     if (!req.body) {
@@ -39,7 +63,7 @@ const registerUser = asyncHandler(async (req, res) => {
 
     const createdUser = await User.findById(user._id).select(
         "-password -refreshToken"
-    )
+    );
 
     if (!createdUser) {
         throw new ApiError(500, "Interal Server Error: Failed to create user");
@@ -50,10 +74,64 @@ const registerUser = asyncHandler(async (req, res) => {
     );
 });
 
-// const registerUser = (req, res) => {
-//     return res.json({
-//         message: "Register route working 🚀",
-//     });
-// };
+const loginUser = asyncHandler(async (req, res) => {
+    // geting the data from the frontend client
+    const { email, phone, password } = req.body;
 
-export { registerUser };
+    // checkinf if the request body is empty or not
+    if (!email && !phone) {
+        throw new ApiError(
+            400,
+            "Bad Request: Email and phone number are required for login"
+        );
+    }
+
+    // the user will give phoneNumber or the email to login to check the user exist or not
+    const user = await User.findOne({
+        $or: [{ email }, { phone }],
+    });
+
+    if (!user) {
+        throw new ApiError(
+            404,
+            "Not Found: User not found with the provided credentials"
+        );
+    }
+
+    // check with the data if the entered password is correct or not
+    const isPasswordValid = await user.comparePassword(password);
+
+    if (!isPasswordValid) {
+        throw new ApiError(401, "Unauthorized: Invalid password");
+    }
+
+    // genereating the access token and refresh token for the user
+    const { accessToken, refreshToken } = await generateTokens(user._id);
+    const loggedInUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    );
+
+    const options = {
+        httpOnly: true,
+        secure: true,
+    };
+
+    // send cookies and response to the frontend client
+    return res
+        .status(200)
+        .cookie("refreshToken", refreshToken, options)
+        .cookie("accessToken", accessToken, options)
+        .json(
+            new ApiResponse(
+                200,
+                {
+                    user: loggedInUser,
+                    accessToken,
+                    refreshToken,
+                },
+                "User Logged in successfully"
+            )
+        );
+});
+
+export { registerUser, loginUser };
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { registerUser } from "../controllers/user.controllers.js";
+import { loginUser, registerUser } from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middleware.js";
 
 const router = express.Router();
 
 router.route("/register").post(upload.none(), registerUser); // using multer to handle multipart/form-data, but since we are not uploading any files, we use upload.none() to parse the form data without expecting any files.
+
+router.route("/login").post(loginUser); // for logging in the user in the web app.
 
 export default router;
